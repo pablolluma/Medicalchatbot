6e00ac064e7b59056f2a0bdc92366d26fd17a2db
pablolluma <hermanopablo1995@gmail.com>
2025-02-10 11:36:11 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot Experimental',
+    version= '0.0.0',
+    author= 'Pablo Lluma',
+    author_email= 'hermanopabo1995@gmail.com',
+    packages= find_packages(),
+    install_requires = [],
+)
+
+
